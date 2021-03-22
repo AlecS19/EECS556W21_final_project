156cfa796cef8355129f0d3fa2f6d6bd6c3ebbec
AlecS19 <alecsoc@umich.edu>
2021-03-22 15:44:54 -0400

    Faster PR, correct pyramid seg, wrong skiier seg

--- ground_truth.jl
+++ ground_truth.jl
@@ -7,11 +7,11 @@ using MAT
 
 #Load Test Image
 curr_dir = pwd()
-filename1 = "227092.jpg"
+filename1 = "260058.jpg"
 filepath1 = curr_dir * "/EECS556W21_final_project/test_images/" * filename1
 
 #Load Ground Truths
-filename2 = "227092.mat"
+filename2 = "260058.mat"
 filepath2 = curr_dir * "/EECS556W21_final_project/ground_truth/" * filename2
 
 file = matopen(filepath2)
@@ -22,24 +22,20 @@ close(file)
 test = channelview(load(filepath1))
 c, m,n = size(test)
 
-#Plot correct imported image
-plot1 = jim( test.*255,title="Original" )
-display(plot1)
-
 #Flatten and run k-means based on intensity
 test_flat = [ vec( test[1,:,:] ) vec( test[2,:,:] ) vec( test[3,:,:] ) ]'
-results = kmeans( test_flat, 3;maxiter = 200,display=:iter)
+results = kmeans( test_flat, 2;maxiter = 200,display=:iter)
 
 segmented = results.assignments
 
 #Format it back into an image
-segmented = reshape( segmented, ( m,n ) )' .*(255/3)
+segmented = reshape( segmented, ( m,n ) ) .*(255/3)
 
 #plot the outcome
-plot2 = jim( segmented, title="segmented")
+plot2 = jim( segmented', title="segmented")
 display(plot2)
-## tet
+## the
 
 include("performance.jl")
 #Display the MIRT
-h =  PR(segmented,correct)
+h =  PR_randindex(segmented,correct)
