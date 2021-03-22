e71192d8df80ffdfff55286a8dfb17dc05f1afff
AlecS19 <alecsoc@umich.edu>
2021-03-22 18:30:03 -0400

    Testing kmeans and gmm. gmm is incorrect

--- ground_truth.jl
+++ ground_truth.jl
@@ -3,8 +3,10 @@ using Clustering
 using MIRT: jim
 using Plots
 using MAT
+using GaussianMixtures
+using LinearAlgebra:I
 
-
+numSegs = 2
 #Load Test Image
 curr_dir = pwd()
 filename1 = "260058.jpg"
@@ -22,9 +24,13 @@ close(file)
 test = channelview(load(filepath1))
 c, m,n = size(test)
 
+## kmeans
 #Flatten and run k-means based on intensity
-test_flat = [ vec( test[1,:,:] ) vec( test[2,:,:] ) vec( test[3,:,:] ) ]'
-results = kmeans( test_flat, 2;maxiter = 200,display=:iter)
+test_flat = [ vec( test[1,:,:] ) vec( test[2,:,:] ) vec( test[3,:,:] ) ]'.*255
+#For some reason scaling to a 255 allows correct segmentation of the skier
+results = kmeans( test_flat, numSegs;init=:rand,tol=1e-6,display=:iter)
+#run kmeans twice to get correct image? Maybe initialization is bad
+#init set to rand seemed to fix the issue
 
 segmented = results.assignments
 
@@ -32,10 +38,29 @@ segmented = results.assignments
 segmented = reshape( segmented, ( m,n ) ) .*(255/3)
 
 #plot the outcome
-plot2 = jim( segmented', title="segmented")
+plot2 = jim( segmented', title="segmented kmeans")
 display(plot2)
+
+## gmm
+data_gmm = Array{Float64,2}(test_flat')
+
+gm = GMM(numSegs,data_gmm;nInit=1,kind=:diag)#full give better accuracy, but not the same picture, when you add extra iterations
+#This seems to give the DGMM results in terms of picture quality and PR ???
+
+gm.μ = results.centers'
+#gm.μ =  Array{Float64}(rand(1:256,(numSegs,3)))
+gm.Σ = ones((numSegs,3))
+
+display( GaussianMixtures.em!(gm,data_gmm;varfloor=1e-5) )
+prob = GaussianMixtures.gmmposterior(gm, data_gmm)[1]
+ass=[argmax(prob[i,:]) for i=1:size(data_gmm,1)]
+segmented_gmm = reshape( ass, ( m,n ) ) .*(255/3)
+
+plot3 = jim(segmented_gmm',"gmm segmented")
+display(plot3)
 ## the
 
 include("performance.jl")
 #Display the MIRT
-h =  PR_randindex(segmented,correct)
+PR_kmeans =  PR_fast(segmented,correct)
+PR_gmm = PR_fast(segmented_gmm,correct)
