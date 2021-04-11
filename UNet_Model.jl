5f530ff5882940437bad47860e60c3e6422ecbc4
najariac <80724410+najariac@users.noreply.github.com>
2021-04-11 13:24:19 -0400

    Add files via upload
    file with function to initialize modified UNet model.

---
+++ UNet_Model.jl
@@ -0,0 +1,73 @@
+using Flux
+
+function concat(mx::AbstractArray{T,4}, x::AbstractArray{T,4}) where T
+    return cat(x, mx, dims = 3)
+end
+
+Create_Unet_Model() = Chain(
+    BatchNorm(1),
+    Conv((3, 3), 1 => 16, pad = 1, relu),
+    BatchNorm(16),
+    Conv((3, 3), 16 => 16, pad = 1, relu),
+    SkipConnection(
+        Chain(
+            MaxPool((2, 2)),
+            BatchNorm(16),
+            Conv((3, 3), 16 => 32, pad = 1, relu),
+            BatchNorm(32),
+            Conv((3, 3), 32 => 32, pad = 1, relu),
+            SkipConnection(
+                Chain(
+                    MaxPool((2, 2)),
+                    BatchNorm(32),
+                    Conv((3, 3), 32 => 64, pad = 1, relu),
+                    BatchNorm(64),
+                    Conv((3, 3), 64 => 64, pad = 1, relu),
+                    SkipConnection(
+                        Chain(
+                            MaxPool((2, 2)),
+                            BatchNorm(64),
+                            Conv((3, 3), 64 => 128, pad = 1, relu),
+                            BatchNorm(128),
+                            Conv((3, 3), 128 => 128, pad = 1, relu),
+                            SkipConnection(
+                                Chain(
+                                    MaxPool((2, 2)),
+                                    BatchNorm(128),
+                                    Conv((3, 3), 128 => 256, pad = 1, relu),
+                                    BatchNorm(256),
+                                    Conv((3, 3), 256 => 256, pad = 1, relu),
+                                    ConvTranspose((2, 2), 256 => 128, stride = 2)
+                                ),
+                                concat
+                            ),
+                            BatchNorm(256),
+                            Conv((3, 3), 256 => 128, pad = 1, relu),
+                            BatchNorm(128),
+                            Conv((3, 3), 128 => 128, pad = 1, relu),
+                            ConvTranspose((2, 2), 128 => 64, stride = 2)
+                        ),
+                        concat
+                    ),
+                    BatchNorm(128),
+                    Conv((3, 3), 128 => 64, pad = 1, relu),
+                    BatchNorm(64),
+                    Conv((3, 3), 64 => 64, pad = 1, relu),
+                    ConvTranspose((2, 2), 64 => 32, stride = 2),
+                ),
+                concat
+            ),
+            BatchNorm(64),
+            Conv((3, 3), 64 => 32, pad = 1, relu),
+            BatchNorm(32),
+            Conv((3, 3), 32 => 32, pad = 1, relu),
+            ConvTranspose((2, 2), 32 => 16, stride = 2),
+        ),
+        concat
+    ),
+    BatchNorm(32),
+    Conv((3, 3), 32 => 16, pad = 1, relu),
+    BatchNorm(16),
+    Conv((3, 3), 16 => 16, pad = 1, relu),
+    Conv((1, 1), 16 => 1, σ)
+)
