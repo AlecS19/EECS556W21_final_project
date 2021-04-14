8b0dd47acca0c2faeb427f32005ec844ca7fa3ba
amakuch00 <78380851+amakuch00@users.noreply.github.com>
2021-04-14 18:49:16 -0400

    Create DGMM_test_medical.jl

---
+++ DGMM_test_medical.jl
@@ -0,0 +1,73 @@
+"""
+#- TEST CODE FOR PNG/JPG/TIF -#
+using Images: load, channelview
+include("performance.jl")
+include("DGMM.jl")
+using MIRT: jim
+using Plots
+
+picnum = "ID_0004"
+filepath1 = "medImage1/"* picnum *".png"
+test = convert(Array{Float64}, load(filepath1)) .* 255
+m, n = size(test)
+test_flat = Array{Float64,2}(reshape(test, 1, :)')
+A = DGMM(test_flat, 2, (m, n))
+E = log_lik(A, test_flat)
+gray_history = train(A, test_flat, 2, 5)
+p1 = plot(gray_history)
+display(p1)
+D = post_prob(A)
+ass = [argmax(D[i, :]) for i = 1:size(D, 1)]
+segmented_gmm = reshape(ass, ( m,n ) )
+jim(segmented_gmm')
+
+"""
+
+#- TEST CODE FOR MED DATASET - JLD2 -#
+using Images: load, channelview
+include("performance.jl")
+include("DGMM.jl")
+using MIRT: jim
+using Plots
+
+picnum = "Test_Set_images"
+filepath1 = picnum *".jld2"
+numsegs = 2
+test = load(filepath1)["test_set_image"][:,:,1,2]
+m, n = size(test)
+test_flat = Array{Float64,2}(reshape(test, 1, :)')
+A = DGMM(test_flat, numsegs, (m, n))
+E = log_lik(A, test_flat)
+gray_history = train(A, test_flat, numsegs, 5)
+p1 = plot(gray_history)
+display(p1)
+D = post_prob(A)
+ass = [argmax(D[i, :]) for i = 1:size(D, 1)]
+segmented_gmm = reshape(ass, ( m,n ) )
+jim(segmented_gmm)
+
+
+
+segmented_gmm = (segmented_gmm) .-1
+segmented_gmm = segmented_gmm .> 0 #flatten the segmentations
+#GT = (abs.(load("medImage1_GT/ID_0006_AGE_0075_CONTRAST_1_CT.png")) ./ 3)'
+picnum = "Test_Set_Label"
+filepath1 = picnum *".jld2"
+GT = load(filepath1)["test_set_label"][:,:,1,2]
+intersection = (segmented_gmm .+ GT) .== 2
+a = 2*sum(intersection) / (sum(segmented_gmm) + sum(GT))
+#test inverted
+segmented_gmm_inverted = segmented_gmm .== 0
+intersection = (segmented_gmm_inverted .+ GT) .== 2
+b = 2*sum(intersection) / (sum(segmented_gmm_inverted) + sum(GT))
+
+if a > b
+    print("Non-inverted Dice score:")
+    print(a)
+    plot(jim(segmented_gmm),jim(GT))
+else
+    print("Inverted Dice score:")
+    print(b)
+    plot(jim(segmented_gmm_inverted),jim(GT))
+end
+
