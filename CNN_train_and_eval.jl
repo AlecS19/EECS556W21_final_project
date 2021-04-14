ce662e01a71fa82f4a38e8c38ecdabdc3b6897a7
najariac <80724410+najariac@users.noreply.github.com>
2021-04-14 18:34:59 -0400

    Add files via upload

--- CNN_train_and_eval.jl
+++ CNN_train_and_eval.jl
@@ -2,34 +2,78 @@ using Flux
 using Flux: @epochs
 using Plots
 using FileIO: load
+using BSON: @save
+using BSON: @load
+using Zygote
+using MIRTjim:jim
+
+##########
+# Load data
+##########
+
+
+Image_path = "Brain_dataset\\"
+
+train_set_image = load(Image_path * "Train_Set_Images.jld2", "train_set_image")
+train_set_label = load(Image_path * "Train_Set_Labels.jld2", "train_set_label")
+test_set_image = load(Image_path * "Test_Set_Images.jld2", "test_set_image")
+test_set_label = load(Image_path * "Test_Set_Label.jld2", "test_set_label")
+
+train_dataset = Flux.Data.DataLoader((train_set_image,train_set_label), batchsize = 4, shuffle = true)
+test_dataset = Flux.Data.DataLoader((test_set_image,test_set_label), batchsize = 2, shuffle = true, partial = false)
+
+##########
+# Set up and train model
+##########
+
+
 include("UNet_Model.jl")
 model_unet = Create_Modified_Unet_Model()
 
-load("medical_dataset\\Train_Set_Images.jld2", "train_set_image")
-load("medical_dataset\\Train_Set_Labels.jld2", "train_set_label")
-load("medical_dataset\\Test_Set_Images.jld2", "test_set_image")
-load("medical_dataset\\Test_Set_Label.jld2", "test_set_label")
-
-
-train_dataset = Flux.Data.DataLoader((train_set_image,train_set_label), batchsize = 16, shuffle = true)
-
-test_dataset = Flux.Data.DataLoader((test_set_image,test_set_label), batchsize = 4, shuffle = true, partial = false)
+#@load "CNN_model.bson" model_unet
 
 function loss(x, y)
          Flux.binarycrossentropy(model_unet(x),y)
- end
+end
 
-test_x, test_y = (test_set_image[:,:,:,1,:],test_set_label[:,:,:,1,:])#
-evalcb() = @show(loss(test_x, test_y))
+test_x, test_y = (test_set_image[:,:,:,1,:],test_set_label[:,:,:,1,:])
 
-@epochs 10 Flux.train!(loss, Flux.params(model_unet), train_dataset, ADAM(), cb = evalcb)
+function evalcb()
+        @show(loss(test_x, test_y))
+        @save "CNN_model.bson" model_unet
+end
 
-p1 = jim(test_set_image[:,:,:,2])
-p2 = jim(test_set_label[:,:,:,2])
-p3 = jim(model_unet(test_set_image[:,:,:,2,:]).>0.5)
+@epochs 30 Flux.train!(loss, Flux.params(model_unet), train_dataset, ADAM(), cb = evalcb)
+
+
+##########
+# View Images and compute accuracies
+##########
+
+include("performance.jl")
+
+viewimage = 5
+
+p1 = jim(test_set_image[:,:,:,viewimage]);
+p2 = jim(test_set_label[:,:,:,viewimage]);
+p3 = jim(model_unet(test_set_image[:,:,:,viewimage,:]).>0.5);
 
 plot(p1,p2,p3)
+@show DICE(model_unet(test_set_image[:,:,:,viewimage,:]).>0.5,test_set_label[:,:,:,viewimage])
 
-#for i in size(test_set_image,4)
+train_dice = 0
+for i in 1:size(train_set_image,4)
+        dice = DICE(model_unet(train_set_image[:,:,:,i,:]).>0.5,train_set_label[:,:,:,i])
+        global train_dice = train_dice + dice
+end
 
-#end
+train_dice = train_dice / size(train_set_image,4)
+@show train_dice
+
+test_dice = 0
+for i in 1:size(test_set_image,4)
+        dice = DICE(model_unet(test_set_image[:,:,:,i,:]).>0.5,test_set_label[:,:,:,i])
+        global test_dice = test_dice + dice
+end
+test_dice = test_dice / size(test_set_image,4)
+@show test_dice
--- partition_medical_image_data.jl
+++ partition_medical_image_data.jl
@@ -1,28 +1,47 @@
 using Images
 using FileIO: save
+using NIfTI
+using GZip
+using MIRTjim:jim
+using Statistics:mean
 
-
-Image_path = "medical_dataset\\Images"
-Label_path = "medical_dataset\\Labels"
+Image_path = "Brain_dataset\\"
 
 filenames = readdir(Image_path)
 
-images = zeros(512,512,1,100)
-labels = zeros(512,512,1,100)
 
-for i in 1:100
-   images[:,:,1,i] = load("medical_dataset\\Images\\" * filenames[i])'
-   labels[:,:,1,i] = abs.(load("medical_dataset\\Label_PNG\\" * filenames[i]))'.>0.001
+test = niread(Image_path * filenames[7])
+test2 = niread(Image_path * filenames[8])
+#test2 = niread(Label_path * filenames[1])
+
+jim(test)
+jim(test2)
+
+images = zeros(256,256,1,39)
+annotations = zeros(256,256,1,7)
+labels = zeros(256,256,1,39)
+
+
+for i in 31:39
+   images[:,:,1,i] = niread(Image_path * "2D_qubiq_fetal_" * lpad(i,2,"0") * "_image.nii")
+   if i == 36
+      labels[:,:,1,i] = niread(Image_path * "2D_qubiq_fetal_" * lpad(i,2,"0") * "_annotation_01.nii")
+   else
+      for j in 1:7
+         annotations[:,:,1,j] = niread(Image_path * "2D_qubiq_fetal_" * lpad(i,2,"0") * "_annotation_0" * string(j) * ".nii")
+      end
+      labels[:,:,1,i] = mean(annotations.>0.1,dims=4).>0.5
+   end
 end
 
-train_set_image = convert(Array{Float32,4}, images[:,:,:,21:100])
-train_set_label = convert(Array{Float32,4}, labels[:,:,:,21:100])
+train_set_image = convert(Array{Float32,4}, images[:,:,:,1:32])
+train_set_label = convert(Array{Float32,4}, labels[:,:,:,1:32])
 
-test_set_image = convert(Array{Float32,4}, images[:,:,:,1:20])
-test_set_label = convert(Array{Float32,4}, labels[:,:,:,1:20])
+test_set_image = convert(Array{Float32,4}, images[:,:,:,33:39])
+test_set_label = convert(Array{Float32,4}, labels[:,:,:,33:39])
 
 
-save("medical_dataset\\Train_Set_Images.jld2", "train_set_image", train_set_image)
-save("medical_dataset\\Train_Set_Labels.jld2", "train_set_label", train_set_label)
-save("medical_dataset\\Test_Set_Images.jld2", "test_set_image", test_set_image)
-save("medical_dataset\\Test_Set_Label.jld2", "test_set_label", test_set_label)
+save(Image_path * "Train_Set_Images.jld2", "train_set_image", train_set_image)
+save(Image_path * "Train_Set_Labels.jld2", "train_set_label", train_set_label)
+save(Image_path * "Test_Set_Images.jld2", "test_set_image", test_set_image)
+save(Image_path * "Test_Set_Label.jld2", "test_set_label", test_set_label)
